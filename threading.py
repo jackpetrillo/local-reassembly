56654529771a8bb840e22ab530a6546ac0270f91
Jack Petrillo <jackpetrillo@gmail.com>
2017-05-09 23:07:11 -0400

    more threading

--- threading.py
+++ threading.py
@@ -6,16 +6,36 @@ CS 321
 """
 
 import debruijn as db
+import randomized_reads as rr
 
 
-
-def thread(variant, genome_db, genome_hash):
+def thread(read, genome_db, genome_hash, gene_k):
     """
 
     """
+    read_kmers = db.kmerList(gene_k, read)
+
+    cur_kmer = read_kmers[0]
+    start_position = -1
+
+    if cur_kmer in genome_hash:
+        start_position = genome_hash[cur_kmer]
+    else:
+        genome_hash[cur_kmer] = 0
+        genome_db[cur_kmer] = [[read_kmers[1]],[1]]
 
 
 
+
+
+def get_Variant(genome, k):
+    """
+
+    """
+    var_seq = rr.rand_variant(genome, 10) #10 is probability
+    read_list = db.kmerList(k, var_seq)
+
+    return read_list[-1] #returns last element of read_list
 
 
 def main():
@@ -23,17 +43,18 @@ def main():
     Main method. Has a sample variant and k. Reads genome from file, creates, a kmerlist,
     creates the genome graph and kmer hash. Threads the sample variant into the db graph.
     """
-    # change this stuff later
-    ##50 chars...ending 3 chars from end of genome. Changing last 3 from AAC to TTT
-    sample_variant = "CCCTCCTTCTCCATCCCTGTAAAATAAATGTAATTTATCTTTGCCAATTT"
-    #constant k
-    k = 25
-
+    gene_k = 25
     gene = db.get_gene("OPN1LW.txt")
-    genome_kmerlist = db.kmerList(k, gene)
+    genome_kmerlist = db.kmerList(gene_k, gene)
     genome_db, kmer_positions = db.create_Debruijn(genome_kmerlist)
 
-    thread(sample_variant, genome_db, kmer_positions)
+    read_k = 50
+    sample_variant = get_Variant(gene, read_k)
+
+    #print(gene)
+    #print(sample_variant)
+
+    thread(sample_variant, genome_db, kmer_positions, gene_k)
 
 
 
