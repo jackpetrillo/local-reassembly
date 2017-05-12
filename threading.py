27596a14559f308034031d7ae6bf766dd44948ba
rgold97 <rosegold@MacBook-Pro-3.local>
2017-05-12 15:19:14 -0400

    maternal_paternal

--- threading.py
+++ threading.py
@@ -7,6 +7,7 @@ CS 321
 
 import debruijn as db
 import randomized_reads as rr
+from random import randint
 
 
 def thread(variant_reads, genome_db, genome_hash, gene_k):
@@ -35,6 +36,26 @@ def thread(variant_reads, genome_db, genome_hash, gene_k):
     return genome_db, genome_hash
 
 
+def uniform_reads(mat, pat, ref, depth, read_len):
+    """
+    """
+    len_seq = len(ref)
+    iterations = (len_seq - read_len + 1) * depth
+    reads = []
+
+
+    for i in range(iterations):
+        mat_or_pat = randint(0, 2)
+        start_ind = randint(0, len_seq - read_len + 1)
+        if mat_or_pat == 0:
+            reads.append(mat[start_ind : start_ind + read_len])
+        else:
+            reads.append(pat[start_ind : start_ind + read_len])
+
+    return reads
+
+
+
 
 def main():
     """
@@ -46,25 +67,33 @@ def main():
     # genome_kmerlist = db.kmerList(gene_k, gene)
     # genome_db, kmer_positions = db.create_Debruijn(genome_kmerlist)
 
-    gene_k = 25
+    gene_k = 15
     prob = 50
     read_length = 50
 
     genome_db, kmer_positions, gene = db.main(gene_k)
 
-    variant1 = rr.rand_variant(gene, prob) #full sequence with SNVs w. probability prob
-    variant1_reads = db.kmerList(read_length, variant1)
+    #variant1 = rr.rand_variant(gene, prob) #full sequence with SNVs w. probability prob
+    #variant1_reads = db.kmerList(read_length, variant1)
+    mat = gene[:-3] + 'Z' + gene[-2:]
+
+    #pat = gene[:-3] + 'Z' + gene[-2:]
+    pat = gene
+
+    reads = uniform_reads(mat, pat, gene, 15, 50)
+
+    genome_db, genome_hash = thread(reads, genome_db, kmer_positions, gene_k)
+    #genome_db, genome_hash = thread(variant1_reads, genome_db, kmer_positions, gene_k)
+
+    #variant2 = rr.rand_variant(gene, prob) #full sequence with SNVs w. probability prob
+    #variant2_reads = db.kmerList(read_length, variant2)
 
 
-    genome_db, genome_hash = thread(variant1_reads, genome_db, kmer_positions, gene_k)
-
-    variant2 = rr.rand_variant(gene, prob) #full sequence with SNVs w. probability prob
-    variant2_reads = db.kmerList(read_length, variant2)
+    #genome_db, genome_hash = thread(variant2_reads, genome_db, kmer_positions, gene_k)
 
 
-    genome_db, genome_hash = thread(variant2_reads, genome_db, kmer_positions, gene_k)
-
-
+    with open("genome_db.txt", "w") as text_file:
+        text_file.write(str(genome_db))
 
     print(genome_db)
 
