7302e03e698a33e22c0768a8cd24cb34f93304be
Jack Petrillo <jackpetrillo@gmail.com>
2017-05-14 14:20:31 -0400

    completely new likelihood

--- likelihood.py
+++ likelihood.py
@@ -6,8 +6,9 @@ CS 321
 """
 import threading as th
 import collections as c
+import numpy as np
 
-def path_scores(genome_db, genome_hash, start_kmer):
+def path_scores(genome_db, genome_hash, ref_gene, start_kmer):
     """
     Traverses all paths and makes a new graph with each paths likelihood scores.
     scores are computed as such:
@@ -15,44 +16,82 @@ def path_scores(genome_db, genome_hash, start_kmer):
     where the transition probability of an edge is computed as the number of reads supporting that
     edge divided by the sum of the support of all edges that share that same source vertex.
     """
-    path_scores = {}
+    keys = genome_db.keys()
+    kmer_size = len(keys[0])
+    num_kmers = len(ref_gene) - kmer_size + 1
+
+    path_kmers = [[] for i in range(num_kmers)]
+    path_scores = [[] for i in range(num_kmers)]
+
 
     start_node = (start_kmer, 1.0) #where we start...
     to_search = c.deque()
-    for k in range(1000):
-        print("")
 
-    i = 0
-    #while start_node[0] in genome_db: #while the node is in the graph (Not the last node)
-    #print len(genome_db.keys())
-    while i < len(genome_db.keys()): #arbitrary? shouldnt be going through keys multiple times
-        #print("RUNNING")
+
+    while(start_node[0] in genome_db):
+        
         node = start_node[0]
+        node_position = genome_hash[node][0] #picking arbitrary first location
+        node_prob = start_node[1]
         denominator = sum(genome_db[node][1])
 
-        for j in range(len(genome_db[node][0])):
-            end_node = genome_db[node][0][j]
-            path_prob = start_node[1]*(genome_db[node][1][j] / float(denominator))
+
+        if node in path_kmers[node_position]:
+            index = path_kmers[node_position].index(node) #get the index
+            path_scores[node_position][index] += node_prob #at that index, update scores with its probability
+        else:
+            path_kmers[node_position].append(node)
+            path_scores[node_position].append(node_prob)
+
+        node_index = path_kmers[node_position].index(node) #this will always be a find.
+
+        for j in range(len(genome_db[node][0])): #for everything it points to
+            end_node = genome_db[node][0][j] #get the child
+            path_prob = path_scores[node_position][node_index] * (genome_db[node][1][j] / float(denominator))
+
             to_search.append((end_node, path_prob))
-            #print("Start "  + node)
-            #print("END " + end_node)
-            #print("Score " + str(path_prob))
-            if node in path_scores:
-                path_scores[node][0].append(end_node)
-                path_scores[node][1].append(path_prob)
-            else:
-                path_scores[node] = [[end_node],[path_prob]]
 
         start_node = to_search.popleft()
 
-        i +=1
+
+    for index in range(len(path_kmers)):
+        print("Position: " + str(index))
+        for j in range(len(path_kmers[index])):
+            print("kmer: " + path_kmers[index][j] + " with probability " + str(path_scores[index][j]))
 
 
-    print(path_scores)
 
-    with open("likelihood_graph.txt", "w") as text_file:
-        text_file.write(str(path_scores))
-    #print(genome_hash["ACGCCTGTAATCCCAGCACTTTGGGAGGC"])
+    #while start_node[0] in genome_db: #while the node is in the graph (Not the last node)
+    # while (i < len(genome_db.keys())) and (start_node[0] in genome_db): #arbitrary? shouldnt be going through keys multiple times
+    #
+    #     node = start_node[0]
+    #     denominator = sum(genome_db[node][1])
+    #     print("The start node is at: " + str(genome_hash[node]))
+    #
+    #     for j in range(len(genome_db[node][0])):
+    #         end_node = genome_db[node][0][j]
+    #         print("It points to nodes at : " + str(genome_hash[end_node]))
+    #
+    #         path_prob = start_node[1]*(genome_db[node][1][j] / float(denominator))
+    #
+    #         to_search.append((end_node, path_prob))
+    #
+    #         if node in path_scores:
+    #             path_scores[node][0].append(end_node)
+    #             path_scores[node][1].append(path_prob)
+    #         else:
+    #             path_scores[node] = [[end_node],[path_prob]]
+    #
+    #     start_node = to_search.popleft()
+    #
+    #     i +=1
+
+
+
+    #print(path_scores)
+
+    # with open("likelihood_graph.txt", "w") as text_file:
+    #     text_file.write(str(path_scores))
 
 
 
@@ -61,11 +100,9 @@ def path_scores(genome_db, genome_hash, start_kmer):
 
 def main():
     genome_db, kmer_positions, gene = th.main()
-    if "CGTGACCCTCAGGT" in genome_db:
-        print("THIS WORKED")
 
 
-    path_scores(genome_db, kmer_positions, "CGTGACCCTCAGGTGATGCGCCAGGGCCGGCTGCCGTCGGGGAC")
+    path_scores(genome_db, kmer_positions, gene, "CGTGACCCTCAGGTGATGCGCCAGGGCCGGCTGCCGTCGGGGAC")
 
 
 
